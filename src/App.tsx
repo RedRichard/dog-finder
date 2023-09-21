1f51e2c3dcbbfa8ece8e11a2bd31577a1a6b8535
Ricardo HG <gomric@hotmail.com>
2023-09-20 22:15:41 -0600

    Basic login fetch setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import React, { useState } from "react";
 
 function App() {
-  const [username, setUsername] = useState<string>();
-  const [password, setPassword] = useState<string>();
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [error, setError] = useState<string>();
+
+  const logIn = async () => {
+    try {
+      const res = await fetch(
+        "https://frontend-take-home-service.fetch.com/auth/login",
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: username,
+            email: email,
+          }),
+        }
+      );
+
+      if (!res.ok) throw new Error("Please check your data");
+    } catch (err: unknown) {
+      if (err instanceof Error) setError(err.message);
+    }
+  };
 
   return (
     <div className="h-screen w-screen flex flex-row items-center justify-center">
@@ -24,16 +48,20 @@ function App() {
               Password:
               <input
                 type="text"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="border-[1px] border-black rounded-md"
               />
             </label>
           </div>
         </form>
-        <button className="bg-slate-500 text-white rounded-md w-full">
+        <button
+          className="bg-slate-500 text-white rounded-md w-full"
+          onClick={logIn}
+        >
           Log In
         </button>
+        {error && <div className="w-full bg-red-500 text-white">{error}</div>}
       </div>
     </div>
   );
