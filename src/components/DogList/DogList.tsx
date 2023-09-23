b2e658b83d5941de3f0b34ae3b9a7e224edd37ab
Ricardo HG <gomric@hotmail.com>
2023-09-23 14:58:15 -0600

    Basic setup for page selector

--- src/components/DogList/DogList.tsx
+++ src/components/DogList/DogList.tsx
@@ -2,19 +2,24 @@ import React, { useState, useEffect } from "react";
 import IDog from "../../interfaces/IDog";
 import DogCard from "../DogCard/DogCard";
 import fetchData from "../../utils/fetchData";
+import IDogSearch from "../../interfaces/IDogSearch";
+
+const DEFAULT_SEARCH_SIZE: number = 25;
 
 const DogList = () => {
-  const [dogIds, setDogIds] = useState<Array<string>>();
+  const [dogSearch, setDogSearch] = useState<IDogSearch>();
   const [dogsData, setDogsData] = useState<Array<IDog>>();
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
   useEffect(() => {
     const getDogIds = async () => {
       try {
-        const res = await fetchData({
+        const res = await fetchData<IDogSearch>({
           endpoint: "/dogs/search",
+          params: { size: "5", from: "0" },
         });
         const dogIds = await res.json();
-        setDogIds(dogIds.resultIds);
+        setDogSearch(dogIds);
       } catch (e) {
         console.error("An error has occurred", e);
       }
@@ -28,7 +33,7 @@ const DogList = () => {
       try {
         const res = await fetchData<Array<string>>({
           endpoint: "/dogs",
-          body: dogIds,
+          body: dogSearch?.resultIds,
         });
         const dogsData = await res.json();
         setDogsData(dogsData);
@@ -37,8 +42,9 @@ const DogList = () => {
       }
     };
 
-    if (dogIds) getDogData();
-  }, [dogIds]);
+    if (dogSearch) getDogData();
+    console.log(`DogList: ${dogSearch?.total}`);
+  }, [dogSearch]);
 
   return (
     <div>
@@ -48,6 +54,25 @@ const DogList = () => {
             <DogCard dogData={dogData} />
           </div>
         ))}
+      <div className="flex flex-row justify-center gap-2">
+        {dogSearch &&
+          [...Array(Math.ceil(dogSearch.total / DEFAULT_SEARCH_SIZE))].map(
+            (x, index) => (
+              <div
+                key={index}
+                className={`h-4 w-4 cursor-pointer ${
+                  (selectedIndex < 4 && index < 9) ||
+                  (index < selectedIndex + 5 && index > selectedIndex - 5)
+                    ? "block"
+                    : "hidden"
+                } ${index === selectedIndex ? "text-red-500" : "text-black"}`}
+                onClick={(e) => setSelectedIndex(index)}
+              >
+                {` ${index + 1}    `}
+              </div>
+            )
+          )}
+      </div>
     </div>
   );
 };
