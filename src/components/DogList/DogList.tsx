37593f16c341244628247305d28c0a73adf79543
Ricardo HG <gomric@hotmail.com>
2023-09-23 19:38:49 -0600

    Moved dog fetch logic to Mobx store

--- src/components/DogList/DogList.tsx
+++ src/components/DogList/DogList.tsx
@@ -3,82 +3,98 @@ import IDog from "../../interfaces/IDog";
 import DogCard from "../DogCard/DogCard";
 import fetchData from "../../utils/fetchData";
 import IDogSearch from "../../interfaces/IDogSearch";
+import { observer } from "mobx-react-lite";
+import { autorun } from "mobx";
+import dogStore from "../../stores/DogStore";
 
 const DEFAULT_SEARCH_SIZE: number = 25;
 
-const DogList = () => {
-  const [dogSearch, setDogSearch] = useState<IDogSearch>();
-  const [dogsData, setDogsData] = useState<Array<IDog>>();
+const DogList = observer(() => {
+  // const [dogSearch, setDogSearch] = useState<IDogSearch>();
+  // const [dogsData, setDogsData] = useState<Array<IDog>>();
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
   useEffect(() => {
-    const getDogIds = async () => {
-      try {
-        const res = await fetchData<IDogSearch>({
-          endpoint: "/dogs/search",
-          params: {
-            size: DEFAULT_SEARCH_SIZE.toString(),
-            from: (selectedIndex * DEFAULT_SEARCH_SIZE).toString(),
-            sort: "breed:asc",
-          },
-        });
-        const dogIds = await res.json();
-        setDogSearch(dogIds);
-      } catch (e) {
-        console.error("An error has occurred", e);
-      }
-    };
+    dogStore.makeDogSearch();
+  }, []);
 
-    getDogIds();
-  }, [selectedIndex]);
+  useEffect(
+    () =>
+      autorun(() => {
+        console.log("autorun");
+        console.log(dogStore.dogsData);
+      }),
+    []
+  );
 
-  useEffect(() => {
-    const getDogData = async () => {
-      try {
-        const res = await fetchData<Array<string>>({
-          endpoint: "/dogs",
-          body: dogSearch?.resultIds,
-        });
-        const dogsData = await res.json();
-        setDogsData(dogsData);
-      } catch (e) {
-        console.error("An error has occurred", e);
-      }
-    };
+  // useEffect(() => {
+  //   const getDogIds = async () => {
+  //     try {
+  //       const res = await fetchData<IDogSearch>({
+  //         endpoint: "/dogs/search",
+  //         params: {
+  //           size: DEFAULT_SEARCH_SIZE.toString(),
+  //           from: (selectedIndex * DEFAULT_SEARCH_SIZE).toString(),
+  //           sort: "breed:asc",
+  //         },
+  //       });
+  //       const dogIds = await res.json();
+  //       setDogSearch(dogIds);
+  //     } catch (e) {
+  //       console.error("An error has occurred", e);
+  //     }
+  //   };
 
-    if (dogSearch) getDogData();
-    console.log(`DogList: ${dogSearch?.total}`);
-  }, [dogSearch]);
+  //   getDogIds();
+  // }, [selectedIndex]);
+
+  // useEffect(() => {
+  //   const getDogData = async () => {
+  //     try {
+  //       const res = await fetchData<Array<string>>({
+  //         endpoint: "/dogs",
+  //         body: dogSearch?.resultIds,
+  //       });
+  //       const dogsData = await res.json();
+  //       setDogsData(dogsData);
+  //     } catch (e) {
+  //       console.error("An error has occurred", e);
+  //     }
+  //   };
+
+  //   if (dogSearch) getDogData();
+  //   console.log(`DogList: ${dogSearch?.total}`);
+  // }, [dogSearch]);
 
   return (
     <div>
-      {dogsData &&
-        dogsData.map((dogData, index) => (
+      {dogStore.dogsData &&
+        dogStore.dogsData.map((dogData, index) => (
           <div key={index}>
             <DogCard dogData={dogData} />
           </div>
         ))}
       <div className="flex flex-row justify-center gap-2">
-        {dogSearch &&
-          [...Array(Math.ceil(dogSearch.total / DEFAULT_SEARCH_SIZE))].map(
-            (x, index) => (
-              <div
-                key={index}
-                className={`h-4 w-4 cursor-pointer ${
-                  (selectedIndex < 4 && index < 9) ||
-                  (index < selectedIndex + 5 && index > selectedIndex - 5)
-                    ? "block"
-                    : "hidden"
-                } ${index === selectedIndex ? "text-red-500" : "text-black"}`}
-                onClick={(e) => setSelectedIndex(index)}
-              >
-                {index + 1}
-              </div>
-            )
-          )}
+        {dogStore.dogSearch &&
+          [
+            ...Array(Math.ceil(dogStore.dogSearch.total / DEFAULT_SEARCH_SIZE)),
+          ].map((x, index) => (
+            <div
+              key={index}
+              className={`h-4 w-4 cursor-pointer ${
+                (selectedIndex < 4 && index < 9) ||
+                (index < selectedIndex + 5 && index > selectedIndex - 5)
+                  ? "block"
+                  : "hidden"
+              } ${index === selectedIndex ? "text-red-500" : "text-black"}`}
+              onClick={(e) => setSelectedIndex(index)}
+            >
+              {index + 1}
+            </div>
+          ))}
       </div>
     </div>
   );
-};
+});
 
 export default DogList;
---
+++ src/stores/DogStore.ts
@@ -0,0 +1,48 @@
+import { makeAutoObservable, autorun } from "mobx";
+import IDogSearch from "../interfaces/IDogSearch";
+import IDog from "../interfaces/IDog";
+import fetchData from "../utils/fetchData";
+
+const DEFAULT_SEARCH_SIZE: number = 25;
+
+class DogStore {
+  selectedIndex: number = 0;
+  dogSearch: IDogSearch = { next: "", resultIds: [], total: 0 };
+  dogsData: Array<IDog> = [];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  *makeDogSearch() {
+    try {
+      // Make dog id search
+      const resSearch: Response = yield fetchData<IDogSearch>({
+        endpoint: "/dogs/search",
+        params: {
+          size: DEFAULT_SEARCH_SIZE.toString(),
+          from: (this.selectedIndex * DEFAULT_SEARCH_SIZE).toString(),
+          sort: "breed:asc",
+        },
+      });
+      const search: IDogSearch = yield resSearch.json();
+      this.dogSearch = search;
+      // console.log(search);
+
+      // Get dog data
+      const resDogs: Response = yield fetchData<Array<string>>({
+        endpoint: "/dogs",
+        body: search.resultIds,
+      });
+      const dogsData: Array<IDog> = yield resDogs.json();
+      // console.log(dogsData);
+      this.dogsData = dogsData;
+    } catch (e) {
+      console.error("An error has occurred", e);
+    }
+  }
+}
+
+const dogStore = new DogStore();
+
+export default dogStore;
