326dfdd07eae3ca31e458607a0c0f1dce632e86f
Ricardo HG <gomric@hotmail.com>
2023-09-24 21:08:43 -0600

    Dog Card Mobile styling

--- src/components/DogCard/DogCard.tsx
+++ src/components/DogCard/DogCard.tsx
@@ -11,7 +11,7 @@ interface IDogCard {
 const DogCard = observer(({ dogData, removable = false }: IDogCard) => {
   return (
     <div
-      className={`border-[2px] m-1 ${
+      className={`p-4 ${
         !removable && dogStore.selectedDogsId[dogData.id]
           ? "bg-red-500 cursor-default"
           : ""
@@ -21,18 +21,39 @@ const DogCard = observer(({ dogData, removable = false }: IDogCard) => {
         if (!removable) dogStore.addSelectedDogId(dogData.id);
       }}
     >
-      <div>{dogData.name}</div>
-      <div>{dogData.age}</div>
-      <div>{dogData.breed}</div>
-      <div>{dogData.zip_code}</div>
-      {removable && (
-        <div
-          className="cursor-pointer"
-          onClick={() => dogStore.deleteSelectedDogId(dogData.id)}
-        >
-          x
+      <div className="p-4 rounded-md bg-vista-blue text-white">
+        <div className="w-full flex flex-row">
+          <div className="basis-[70%] flex flex-row p-2 justify-between">
+            <div>
+              <div className="font-semibold">Name</div>
+              <div className="font-semibold">Age</div>
+              <div className="font-semibold">Breed</div>
+              <div className="font-semibold">Zip Code</div>
+            </div>
+            <div className="text-right">
+              <div>{dogData.name}</div>
+              <div>{dogData.age}</div>
+              <div>{dogData.breed}</div>
+              <div>{dogData.zip_code}</div>
+            </div>
+          </div>
+          <div className="basis-1/3 p-1">
+            <img
+              src={dogData.img}
+              alt={`${dogData.name}'s photo`}
+              className="object-cover h-full w-full rounded-xl"
+            />
+          </div>
         </div>
-      )}
+        {removable && (
+          <div
+            className="cursor-pointer"
+            onClick={() => dogStore.deleteSelectedDogId(dogData.id)}
+          >
+            x
+          </div>
+        )}
+      </div>
     </div>
   );
 });
