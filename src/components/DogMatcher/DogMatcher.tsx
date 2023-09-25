b7ecd4119fb2b14652e9dd86bafeb9a37764bf62
Ricardo HG <gomric@hotmail.com>
2023-09-24 22:17:39 -0600

    Match button styling

--- src/components/DogMatcher/DogMatcher.tsx
+++ src/components/DogMatcher/DogMatcher.tsx
@@ -5,13 +5,15 @@ import DogCard from "../DogCard/DogCard";
 
 const DogMatcher = observer(() => {
   return (
-    <div>
-      <button
-        className="bg-blue-500 text-white rounded-md"
-        onClick={() => dogStore.makeMatch()}
-      >
-        Match
-      </button>
+    <div className="p-4">
+      <div className="flex flex-row justify-end">
+        <button
+          className="bg-cornflower-blue p-4 w-full md:w-1/3 lg:w-1/4 font-semibold text-white rounded-md"
+          onClick={() => dogStore.makeMatch()}
+        >
+          Match!
+        </button>
+      </div>
       {dogStore.matchedDogData && <DogCard dogData={dogStore.matchedDogData} />}
     </div>
   );
