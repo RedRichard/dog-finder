ee6a613d4d8c8ced94b03133492afdd805416e7a
Ricardo HG <gomric@hotmail.com>
2023-09-24 22:50:40 -0600

    Fix

--- src/components/SortBreedSelector/SortBreedSelector.tsx
+++ src/components/SortBreedSelector/SortBreedSelector.tsx
@@ -14,8 +14,8 @@ const SortBreedSelector = observer(() => {
           onChange={(e) => searchFiltersStore.setSortBreed(e.target.value)}
           className="w-full md:w-auto border-[1px] border-black rounded-md border-none text-black p-2"
         >
-          <option value="asc">Breed Asc</option>
-          <option value="desc">Breed Desc</option>
+          <option value="asc">Breed ASC</option>
+          <option value="desc">Breed DESC</option>
         </select>
       </label>
     </div>
