a8a45c4fa133b487fa6c44798a553f45a1a55847
Ricardo HG <gomric@hotmail.com>
2023-09-24 20:51:45 -0600

    AgeSelector component

--- src/components/DogAgeSelector/DogAgeSelector.tsx
+++ src/components/DogAgeSelector/DogAgeSelector.tsx
@@ -1,43 +1,21 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
 import searchFiltersStore from "../../stores/SearchFiltersStore";
+import AgeSelector from "../AgeSelector/AgeSelector";
 
 const DogAgeSelector = observer(() => {
   return (
     <div className="flex flex-row gap-4 justify-between md:justify-start">
-      <label htmlFor="dog-age-min" className="text-white">
-        Min Age
-        <select
-          id="dog-age-min"
-          name="dog-age-min"
-          value={searchFiltersStore.minAge}
-          onChange={(e) =>
-            searchFiltersStore.setMinAge(parseInt(e.target.value))
-          }
-          className="ml-2 border-[1px] border-black rounded-md border-none text-black p-2"
-        >
-          {[...Array(20)].map((_, index) => (
-            <option key={index}>{index}</option>
-          ))}
-        </select>
-      </label>
-      <label htmlFor="dog-age-max" className="text-white">
-        {" "}
-        Max Age
-        <select
-          id="dog-age-max"
-          name="dog-age-max"
-          value={searchFiltersStore.maxAge}
-          onChange={(e) =>
-            searchFiltersStore.setMaxAge(parseInt(e.target.value))
-          }
-          className="ml-2 border-[1px] border-black rounded-md border-none text-black p-2"
-        >
-          {[...Array(20)].map((_, index) => (
-            <option key={index}>{index}</option>
-          ))}
-        </select>
-      </label>
+      <AgeSelector
+        id="dog-age-min"
+        selectValue={searchFiltersStore.minAge}
+        onChangeHandler={searchFiltersStore.setMinAge}
+      />
+      <AgeSelector
+        id="dog-age-max"
+        selectValue={searchFiltersStore.maxAge}
+        onChangeHandler={searchFiltersStore.setMaxAge}
+      />
     </div>
   );
 });
