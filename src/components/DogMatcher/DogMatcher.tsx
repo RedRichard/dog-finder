e39223f54c48356212036aeb693aefd17e55e877
Ricardo HG <gomric@hotmail.com>
2023-09-24 22:52:47 -0600

    Text fix

--- src/components/DogMatcher/DogMatcher.tsx
+++ src/components/DogMatcher/DogMatcher.tsx
@@ -16,7 +16,9 @@ const DogMatcher = observer(() => {
         </button>
       </div>
 
-      <div className="font-bold text-xl pt-10 pb-4">{`Meet ${dogStore.matchedDogData?.name}, your new best friend!`}</div>
+      {dogStore.matchedDogData && (
+        <div className="font-bold text-xl pt-10 pb-4">{`Meet ${dogStore.matchedDogData?.name}, your new best friend!`}</div>
+      )}
       <div className="flex flex-row justify-center">
         <div className="w-full md:w-1/2 lg:w-1/3">
           {dogStore.matchedDogData && (
